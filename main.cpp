bac4f46b43c5d2d0d464c41fe2e4925a79e3e134
Vladimir Pan <st128934@student.spbu.ru>
2024-09-18 15:48:14 +0300

    adding main.cpp that was deleted be error

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*Pan Vladimir st128934@student.spbu.ru
+  modified hello world
+*/
+
+#include "header.h"
+#include <iostream>
+#include <string>
+
+int main(){
+	std::string word  = "world";
+	print(word);
+}
+
