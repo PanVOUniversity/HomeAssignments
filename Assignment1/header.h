99b5ca88af366003f749f80b1a0bcd1ca25adcad
Vladimir Pan <st128934@student.spbu.ru>
2024-09-27 12:30:31 +0300

    Init new dir

---
+++ Assignment1/header.h
@@ -0,0 +1,11 @@
+/*Pan Vladimir st128934@student.spbu.ru
+  modified hello world
+*/
+
+#include <string>
+#ifndef HEADER_h
+#define HEADER_h
+
+void print(std::string);
+
+#endif
---
+++ Assignment1/makefile
@@ -0,0 +1,14 @@
+final: main.o func.o
+	g++ main.o func.o -o hello
+
+main.o: main.cpp
+	g++ -c main.cpp 
+
+func.o: func.cpp
+	g++ -c func.cpp
+
+clean:
+	rm *.o
+
+
+
