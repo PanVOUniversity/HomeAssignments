1d49f76e5a29cd8b7b8d9abd99d98776857f2e77
Vladimir Pan <st128934@student.spbu.ru>
2024-10-03 22:00:09 +0300

    Done: Выделить под него массив Считать файл в массив, Развернуть массив, Записать в новый файл одной командо

--- Assignment2a/main.cpp
+++ Assignment2a/main.cpp
@@ -2,19 +2,46 @@
 #include <fstream>
 #include <string>
 #include <filesystem>
-std::ifstream infile;
+#include <algorithm>
 
 int main()
 	{
+		//input path
 		std::string a;
 		std::cout << "input path, pls \n";
 		std::cin >> a;
-
+		
 		//find out the size of binary file
 		std::filesystem::path filepath = a;
 		auto b = std::filesystem::file_size(filepath);
 		std::cout <<  b << "\n";
+		std::ifstream infile;
+		infile.open(a ,std::ios::binary|std::ios::in);
 		
-		//infile.open(a ,std::ios::binary|std::ios::in);
+		//open file, write it into the array, close it, delete it
+		infile.open(a ,std::ios::binary|std::ios::in);
+		char buffer [b];
+		if (infile.is_open())
+		{
+			infile.read((char *)&buffer,sizeof(buffer));
+			infile.close();
+			
+		}	
 		
+		//naive reverse realisation
+		int buffersize = sizeof(buffer)/sizeof(buffer[0]);
+		for (int i = 0;i < buffersize; i++)
+		{
+			std::swap(buffer[i], buffer[buffersize - i]);
+			
+		}
+		
+	       //writing array in new file
+
+		std::ofstream("reversed.txt") << buffer;
+
+		//deleting links
+
+		
+			 
 	}
