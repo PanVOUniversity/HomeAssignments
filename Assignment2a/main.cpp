668513af55418543deab45c79b4097bed172dc12
Vladimir Pan <st128934@student.spbu.ru>
2024-10-03 10:32:38 +0300

    completed define size task

--- Assignment2a/main.cpp
+++ Assignment2a/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <filesystem>
 std::ifstream infile;
 
 int main()
 	{
 		std::string a;
+		std::cout << "input path, pls \n";
 		std::cin >> a;
-		auto b = sizeof(a);
-		std::cout <<  b;
-		std::ofstream fout(txt.bin, ios_base::binary); //create class ofstream object
-		string h = infile.open(a ,std::ios::binary|std::ios::in);
+
+		//find out the size of binary file
+		std::filesystem::path filepath = a;
+		auto b = std::filesystem::file_size(filepath);
+		std::cout <<  b << "\n";
+		
+		//infile.open(a ,std::ios::binary|std::ios::in);
 		
 	}
