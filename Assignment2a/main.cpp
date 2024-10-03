447fcfa7f64c08c46a44dcc751a539b0463059e0
Vladimir Pan <st128934@student.spbu.ru>
2024-10-03 09:31:51 +0300

    init folder, create main.cpp, testing functions

---
+++ Assignment2a/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+std::ifstream infile;
+
+int main()
+	{
+		std::string a;
+		std::cin >> a;
+		auto b = sizeof(a);
+		std::cout <<  b;
+		std::ofstream fout(txt.bin, ios_base::binary); //create class ofstream object
+		string h = infile.open(a ,std::ios::binary|std::ios::in);
+		
+	}
