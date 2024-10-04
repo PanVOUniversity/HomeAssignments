17092e348e26ce89aa4d3ff96e0ac44f49dca770
Vladimir Pan <st128934@student.spbu.ru>
2024-10-04 18:26:49 +0300

    all_done

--- Assignment2b/main.cpp
+++ Assignment2b/main.cpp
@@ -2,16 +2,44 @@
 
 int main()
 	{
-		char *stack[100] = new char;
+		char *stack[100] = new char[100];
 		char operations[] = {"+","-","*","/"};
 		
 		while (true)
+		{
+			for(k == 0, k<100, ++k)
 			{
 				std::cin >> s;
 				for (i=0, i<4, i++)
 					{
 						if (operations[i] == s)
 						{
-							
+							int sum;
+							for (j = 0; i < sizeof(stack)/ sizeof(stack);++j)
+							{
+								if (i=0)
+								{ 
+									sum += stack[j] - 0;
+								}
+								else if (i = 1)
+								{
+									sum -= stack[j] - 0;
+								}
+								else if (i = 2)
+								{
+									sum *= stack[j] - 0;
+								}
+								else if (i = 3)
+								{
+									sum /= stack[j] - 0;
+								}
+							}
+						}
+					}
+				else
+		{
+					
+				}		
+										
 							break;
 	}	
