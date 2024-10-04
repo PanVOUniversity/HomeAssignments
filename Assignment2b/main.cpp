d0712b6cb9b6a0a9badee1ff0f6b7d1882b1a266
Vladimir Pan <st128934@student.spbu.ru>
2024-10-04 10:44:16 +0300

    added folder, done first steps in main

---
+++ Assignment2b/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+	{
+		char *stack[100] = new char;
+		char operations[] = {"+","-","*","/"};
+		
+		while (true)
+			{
+				std::cin >> s;
+				for (i=0, i<4, i++)
+					{
+						if (operations[i] == s)
+						{
+							
+							break;
+	}	
