d00fbde5980dfd6c4ca2e662f42fe143dc2969fc
PanVladimir <st128934@student.spbu.ru>
2024-11-05 22:55:43 +0300

    adding derived Class

--- Assignment3/scope.cpp
+++ Assignment3/scope.cpp
@@ -1,3 +1,13 @@
 #include "Scope.h"
 #include <iostream>
 
+Scope::Scope(int type):Weapon;
+
+void Scope::setScope(const std::string& new_scope) {
+    zoom = new_scope;
+}
+
+void Scope::getType() const {
+    Weapon::getType();
+    std::cout << "Zoom is " << zoom<<"x";
+}
