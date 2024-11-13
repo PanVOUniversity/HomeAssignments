4eede8d4c1b47bb628a5223814492d28930ad696
Vladimir Pan <st128934@student.spbu.ru>
2024-11-13 16:20:14 +0300

    save all before rewriting

---
+++ Assignment3/Vehicle.h
@@ -0,0 +1,19 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <string>
+
+class Vehicle {
+public:
+    Vehicle(const std::string& type, const std::string& model)
+        : type(type), model(model) {}
+
+    std::string getType() const { return type; }
+    std::string getModel() const { return model; }
+
+private:
+    std::string type;
+    std::string model;
+};
+
+#endif
