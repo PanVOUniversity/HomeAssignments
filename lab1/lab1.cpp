bded5693b58277dc614fe3df89af69139220d8c5
Vladimir Pan <st128934@student.spbu.ru>
2024-11-25 20:56:53 +0300

    correct writing bmp without padding and reading raw file

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,38 +1,24 @@
-#include <fstream>
-#include <vector>
-#include <iostream>
-#include <string>
-#include <tuple>
-#include <lab1.h>
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include "bmp.h"
 
-std::tuple <std::vector<char>, std::streamsize> readFileAsBinary(const std::string& filename) {
-    std::ifstream file(filename, std::ios::binary | std::ios::ate); // places the read position at the end 
-    if (!file) throw std::runtime_error("Could not open file"); 
 
-    std::streamsize size = file.tellg(); 
-    file.seekg(0, std::ios::beg); //places the read position to 0
-
-    std::vector<char> buffer(size);
-    if (!file.read(buffer.data(), size)) throw std::runtime_error("Could not read file");
-
-    return make_tuple(buffer,size);
-}
-
-int main() {
-    std::string path;
-    std::cin >> path;
-    std::tuple<std::vector<char>, std::streamsize> result = readFileAsBinary(path); // read file
-    std::vector<char> buffer = std::get<0>(result);
-    std::streamsize size = std::get<1>(result);
-    char* bin_img = buffer.data(); //allocating in memory
+void test()
+{
     
-    //writing array into file
-    std::ofstream file("output.txt");
-
-    for (size_t i = 0; i < buffer.size(); ++i) {
-        file << bin_img[i];
-        if (i != buffer.size() - 1) {
-            file << " ";  // Add space between elements
+    std::string path = "lena_gray.raw";
+    Image image = Image(512, 512, path);
+    std::vector<unsigned char> vector = image.readToVector(path, 512*512);
+    for(int i = 0; i<= 39; i++)
+    {
+        std::cout<<vector[i]<<std::endl;
     }
+    image.writeToFile("new.bmp",vector);
 }
 
+int main()
+{
+    test();
+}
