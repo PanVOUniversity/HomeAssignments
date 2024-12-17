2cfb3d408672aa48d9033f873e1857a1dc17cd50
Vladimir Pan <st128934@student.spbu.ru>
2024-12-17 11:17:21 +0300

    all done, without tests

---
+++ Assignment5/Autobot.h
@@ -0,0 +1,45 @@
+/*Pan Vladimir st128934@student.spbu.ru
+"Home assignment 4 - operator and constructors overloading"
+*/
+#ifndef AUTOBOT
+#define AUTOBOT
+
+#include "Transformer.h"
+
+
+class Autobot:public Transformer
+{
+private:
+    std::string _commander;
+    std::string _reason_for_kindness;
+
+public:
+    Autobot();
+    Autobot(
+        const std::string &name,
+        const uint &power,
+        const uint &speed,
+        const int &advantage_power,
+        const std::string &commander,
+        const std::string &reason_for_kindness
+    );
+    ~Autobot();
+
+    std::string getCommander() const;
+    void setCommander(const std::string &commander);
+
+    std::string getReasonForKindness() const;
+    void setReasonForKindness(const std::string &reason_for_kindness);
+
+    void callComander();
+
+    void transform() override;
+    void openFire() override;
+    void ulta() override;
+
+    
+};
+
+std::ostream& operator<<(std::ostream& os, const Autobot& item);
+
+#endif
---
+++ Assignment5/TransformerTest.cpp
@@ -0,0 +1,12 @@
+/*Pan Vladimir st128934@student.spbu.ru
+"Home assignment 4 - operator and constructors overloading"
+*/
+// TransformerTest.cpp
+#include <gtest/gtest.h>
+#include "City.h"
+#include "Transformer.h"
+#include "Advantage.h"
+#include "Autobot.h"
+#include "Minicon.h"
+#include "Decepticon.h"
+
